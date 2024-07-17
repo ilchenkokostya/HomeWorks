c540b7ac882136cdddf018139f173cffc6eb77d9
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-07-17 21:02:15 +0300

    vers 2.0.1

---
+++ Homework_2.py
@@ -0,0 +1,7 @@
+Number_of_completed_HW = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+print(f'Курс: {Course_name}, '
+      f'всего задач: {Number_of_completed_HW}, '
+      f'затрачено часов: {Number_of_hours_spent}, '
+      f'среднее время выполнения {Number_of_hours_spent / Number_of_completed_HW} часа.')

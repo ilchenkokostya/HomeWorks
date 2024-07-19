3b08dcebd458fc10e3d56ecb28ac78bd594c0183
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-07-19 21:36:39 +0300

    vers 1.0.1

---
+++ Homework_5.py
@@ -0,0 +1,15 @@
+immutable_var = 5,7,6,True,"Privet"
+print(immutable_var)
+
+try:
+    immutable_var[0] = 'figvam'
+except :
+    print("Кортежи в Python являются неизменяемыми структурами данных")
+
+mutable_list = [11,12,True,"Yes"]
+print(mutable_list)
+mutable_list[0] = 1
+mutable_list[1] = 2
+mutable_list[2] = False
+mutable_list[3] = "No"
+print(mutable_list)

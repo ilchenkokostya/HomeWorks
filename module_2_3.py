13664e66b95c1a16face3beefd354aea74119dba
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-07-25 10:23:05 +0300

    vers 1.0.1

---
+++ module_2_3.py
@@ -0,0 +1,20 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+i = -1
+
+while i < len(my_list)-1:
+    i += 1
+
+    if my_list[i] > 0:
+        print(my_list[i])
+        continue
+
+    if my_list[i] < 0:
+        print('Встретилось отрицательное число:', my_list[i])
+        break
+
+
+
+
+
+

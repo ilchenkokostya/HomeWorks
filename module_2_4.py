0b681d089c69c5140b229f7e1b0680b72eeb4795
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-07-25 23:21:49 +0300

    vers 1.0.1

--- module_2_4.py
+++ module_2_4.py
@@ -3,21 +3,22 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
-for num in numbers:
-    if num == 1:
+for n in numbers:
+    if n == 1:
+        # print(n, '- не является ни простым, ни составным числом')
         continue
 
     is_prime = True
 
-    for i in range(2, num):
-        if num % i == 0:
+    for i in range(2, n):
+        if n % i == 0:
             is_prime = False
             break
 
     if is_prime:
-        primes.append(num)
+        primes.append(n)
     else:
-        not_primes.append(num)
+        not_primes.append(n)
 
 print("Primes:", primes)
 print("Not Primes:", not_primes)

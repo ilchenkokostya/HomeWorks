760e4f3124ff2860a64830f18e1220a6bb93836a
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-07-29 09:12:09 +0300

    vers 1.0.1

---
+++ module_2_5.py
@@ -0,0 +1,20 @@
+def get_matrix(n, m, value):
+    matrix = []
+
+    # for i in range(n):
+    #    matrix.append([value]*j)
+
+    for i in range(n):
+        matrix_2 = []
+        for j in range(m):
+           matrix_2.append(value)
+        matrix.append(matrix_2)
+
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)

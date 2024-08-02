47e827fdfcaf766478c90bd5714e1f4165754869
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-08-02 13:55:28 +0300

    vers 1.0.1

--- module_3_4.py
+++ module_3_4.py
@@ -1,11 +1,11 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for word in other_words:
-        if (
+        if (                    # 1 #
                 # str(root_word).lower() in str(word).lower()
                 # or
                 # str(word).lower() in str(root_word).lower()
-
+                                # 2 #
                 str(root_word).lower().count(str(word).lower())
                 or
                 str(word).lower().count(str(root_word).lower())
@@ -14,8 +14,7 @@ def single_root_words(root_word, *other_words):
     return same_words
 
 
-print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
-# ['richiest', 'richies']
-
-print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
-# ['Disable']
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)

c122b441967bee467ab3bc82233aa92ec927b7a3
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-08-08 20:20:03 +0300

    vers 1.0.2

---
+++ module_4_1/fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second:
+        return first / second
+    else:
+        return "Division by zero is not allowed"
---
+++ module_4_1/true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+
+def divide(first, second):
+    if second:
+        return first / second
+    else:
+        return inf

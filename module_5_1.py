fcb7dd216ef5f4281cad0769608fcf92d4b50f66
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-08-21 14:51:31 +0300

    vers 1.0.2

--- module_5_1.py
+++ module_5_1.py
@@ -13,7 +13,8 @@ class House:
             print(f"Такого этажа не существует в {self.name}")
 
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+if __name__ == '__main__':
+    h1 = House('ЖК Горский', 18)
+    h2 = House('Домик в деревне', 2)
+    h1.go_to(5)
+    h2.go_to(10)

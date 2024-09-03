d6c508bb643dc11c77ccce58d76d7d386ede24f0
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-09-03 23:04:13 +0300

    vers 1.0.1

---
+++ module_6_3.py
@@ -0,0 +1,42 @@
+class Horse:  # класс, описывающий лошадь
+    def __init__(self, x_distance=0, sound='Frr'):
+        self.x_distance = x_distance  # пройденный пут
+        self.sound = sound  # звук, который издаёт лошадь.
+        super().__init__()
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:  # класс описывающий орла
+    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
+        self.y_distance = y_distance  # высота полёта
+        self.sound = sound  # звук, который издаёт орёл
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus(Horse, Eagle):  # класс описывающий пегаса
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):  # метод, возвращающий позицию пегаса
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        return print(self.sound)
+
+
+if __name__ == '__main__':
+    p1 = Pegasus()
+
+    print(p1.get_pos())
+    p1.move(10, 15)
+    print(p1.get_pos())
+    p1.move(-5, 20)
+    print(p1.get_pos())
+
+    p1.voice()

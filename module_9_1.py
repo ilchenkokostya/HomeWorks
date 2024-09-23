965ad381f6326d1cd3f0674722e65ad23024029e
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-09-23 22:40:23 +0300

    vers 1.0.1

--- module_9_1.py
+++ module_9_1.py
@@ -15,4 +15,9 @@ if __name__ == "__main__":
         return list(result)
 
 
-    print(apply_all_func([6, 20, 15, 9], squaring))
+    def not_even(int_list):
+        result = filter(lambda x: x % 2 != 0, int_list)
+        return list(result)
+
+
+    print(apply_all_func([6, 20, 15, 9], squaring, not_even))

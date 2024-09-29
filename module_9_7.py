df1ec05551cc084c9cc7009291e91f073aa195e0
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-09-29 23:17:19 +0300

    vers 1.0.1

---
+++ module_9_7.py
@@ -0,0 +1,23 @@
+# Задание: Декораторы в Python
+def is_prime(func):
+    def wrapper(a, b, c):
+        suma = func(a, b, c)
+        if suma > 1:
+            if any((suma % i == 0) for i in range(2, int(suma ** 0.5) + 1)):
+                return f'Составное\n {suma}'
+            else:
+                return f'Простое\n {suma}'
+        else:
+            return f'Входные параметры: натуральные числа больше единицы'
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+if __name__ == '__main__':
+    result = sum_three(2, 3, 6)
+    print(result)

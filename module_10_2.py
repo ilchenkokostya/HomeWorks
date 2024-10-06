d360f1f1cae7406b3251ad9ff807bc9f8f1a7f2e
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-10-06 22:55:49 +0300

    vers 1.0.1

--- module_10_2.py
+++ module_10_2.py
@@ -10,18 +10,18 @@ class Knight(Thread):
         self.name = name
         self.power = power
         self.days = 0
-        self.bots = 100
+        self.warriors = 100
 
     def run(self):
         print(f"{self.name}, на нас напали!")
 
-        while self.bots > 0:
-            self.bots -= self.power
+        while self.warriors > 0:
+            self.warriors -= self.power
             self.days += 1
             time.sleep(1)
-            if self.bots <= 0:
+            if self.warriors <= 0:
                 break
-            print(f"{self.name} сражается {self.days}, осталось {self.bots} воинов.")
+            print(f"{self.name} сражается {self.days}, осталось {self.warriors} воинов.")
         print(f"{self.name} одержал победу спустя {self.days} дней(дня)!")
 
 
@@ -33,5 +33,3 @@ second_knight.start()
 
 first_knight.join()
 second_knight.join()
-
-print("Все враги повержены!!!")

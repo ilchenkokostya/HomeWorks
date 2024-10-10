d695d6d2dea86cf488a3d023431bce5f2e0c8e12
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-10-10 21:55:44 +0300

    vers 1.0.1

--- module_9_3.py
+++ module_9_3.py
@@ -4,9 +4,12 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) - len(y))
-second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
-
 print(list(first_result))
+
+first_result = next(len(x) - len(y) for x, y in zip(first, second) if len(x) - len(y))
+print(first_result)
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 print(list(second_result))
 
 third_result = (len(value) == len(second[i]) for i, value in enumerate(first))

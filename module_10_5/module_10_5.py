7c77516bcc2efb8ca5030959d2484052ec76e9ce
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-10-12 19:49:31 +0300

    vers 1.0.1

--- module_10_5/module_10_5.py
+++ module_10_5/module_10_5.py
@@ -1,3 +1,4 @@
+# Многопроцессное программирование
 import datetime
 import multiprocessing
 
@@ -10,12 +11,14 @@ def read_info(name):
 
 
 if __name__ == '__main__':
+    # Линейный вызов
     start = datetime.datetime.now()
     for i in range(1, 5):
         read_info(f'file {i}.txt')
     end = datetime.datetime.now()
     print(end - start, '(линейный)')
 
+    # Многопроцессный
     filenames = [f'./file {number}.txt' for number in range(1, 5)]
     with multiprocessing.Pool(processes=4) as pool:
         start = datetime.datetime.now()

c590f4aed705e0860cd5e2031862590e718b7210
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-12-16 21:22:15 +0300

    vers 1.0.1

---
+++ module_16_5/module_16_5.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field, ConfigDict
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+users = []
+
+
+class User(BaseModel):
+    id: int = Field(ge=1, description="Enter User ID")
+    username: str = Field(min_length=5, max_length=20, description="Enter username")
+    age: int = Field(ge=18, le=120, description="Enter age")
+    # запрет на доп поля
+    model_config = ConfigDict(extra='forbid')
+
+
+@app.get("/user")
+async def get_users() -> list:
+    return users
+
+
+@app.post("/user/{username}/{age}")
+async def post_user(user: User) -> list:
+    user.id = max((i.id for i in users), default=0) + 1
+    users.append(user)
+    return users
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def put_user(user_id: int, user: User) -> list:
+    for i in users:
+        if i.id == user_id:
+            i.username = user.username
+            i.age = user.age
+            return users
+    else:
+        raise HTTPException(status_code=404, detail="User ID not found")
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: int) -> list:
+    for i, t in enumerate(users):
+        if t.id == user_id:
+            del users[i]
+            return users
+    else:
+        raise HTTPException(status_code=404, detail="User ID not found")
+
+
+if __name__ == '__main__':
+    uvicorn.run("module_16_5:app", host='127.0.0.1', port=8000, reload=True)
